07babc7fca8bd98e57a0d7d8867eafc6779c66b2
rajeshpiyer <prajeshiyer@gmail.com>
2022-05-31 22:03:11 +0530

    $new

---
+++ form.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+        import os
+        import pandas as pd
+        import numpy as np
+
+        dataset = pd.read_csv("ageandheight.csv")
+        x = dataset['Age'].values
+        y = dataset['Height'].values
+
+        from sklearn.preprocessing import PolynomialFeatures
+        x = x.reshape(-1, 1)
+        poly = PolynomialFeatures(degree=1)
+        X_poly = poly.fit_transform(x)
+        poly.fit(X_poly, y)
+    
+        from sklearn.linear_model import LinearRegression
+        regressor = LinearRegression()
+        regressor.fit(X_poly, y)
+
+        y_pred = regressor.predict(X_poly)
+
+        form_data = request.form
+
+        input1 = form_data.get("age")
+        Xnew = [[input1]]
+        result = regressor.predict(Xnew)
+        value = result.tolist()
+        final="The Predicted height is" + str(value[0])
+        return final
+
+    else:
+         return render_template("form.html")
+
+if __name__=='__main__':
+   app.run()
